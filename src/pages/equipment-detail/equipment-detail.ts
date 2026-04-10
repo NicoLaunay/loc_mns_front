388f7f2da953ecae0a860fce203e06961d277e28
Nicolas <nicolas.launay.0@gmail.com>
2026-04-10 16:04:26 +0200

    NEW: equipment detail page added

---
+++ src/pages/equipment-detail/equipment-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'equipment-detail',
+  imports: [],
+  templateUrl: './equipment-detail.html',
+  styleUrl: './equipment-detail.css',
+})
+export class EquipmentDetail {}
