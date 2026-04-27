7bcdbec30b9b61b1cb2a2b82b32533ac41f612f5
Nicolas <nicolas.launay.0@gmail.com>
2026-04-27 16:54:41 +0200

    NEW(request-form): added form for admin request

---
+++ src/app/pages/request-form/request-form.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'request-form',
+  imports: [RouterLink],
+  templateUrl: './request-form.html',
+  styleUrl: './request-form.css',
+})
+export class RequestForm {}
