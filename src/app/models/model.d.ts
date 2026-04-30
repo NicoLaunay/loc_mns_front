2ba29ffeab0efe09c68cdeb8a0620ccf95df1a77
Nicolas <nicolas.launay.0@gmail.com>
2026-04-30 13:43:49 +0200

    NEW(models): added model  object model

---
+++ src/app/models/model.d.ts
@@ -0,0 +1,11 @@
+import { Type } from "./type";
+
+type Model = {
+    id: Number;
+    name: String;
+    description: String;
+    type: Type;
+    isComponent: Boolean;
+    documentations: Array<Documentation>;
+    components: Array<Model>;
+}
