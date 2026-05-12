16b3e2b42127c541ec0353bf901beb889d09d281
Nicolas <nicolas.launay.0@gmail.com>
2026-05-12 14:52:05 +0200

    NEW(data-class-enum): new enum containing all class types from database

---
+++ src/app/models/data-class.enum.ts
@@ -0,0 +1,14 @@
+export enum DataClass {
+    TYPES = "Types",
+    MODELS = "Models",
+    EQUIPMENTS = "Equipments",
+    USERS = "Users",
+    ACCREDITATIONS = "Accreditations",
+    ROLES = "Roles",
+    DOCUMENTATIONS = "Documentations",
+    LOCATIONS = "Locations",
+    STATES = "States",
+    MODIFICATIONS = "Modifications",
+    LOANS = "Loans",
+    REQUESTS = "Requests"
+}
