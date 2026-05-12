03598f2de61fb10949dbb5286ec496c4185c40f1
Nicolas <nicolas.launay.0@gmail.com>
2026-05-12 14:50:49 +0200

    FIX(equipment-detail): added Routerlink import

--- src/app/pages/equipment-detail/equipment-detail.ts
+++ src/app/pages/equipment-detail/equipment-detail.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { Equipment } from '../../services/equipment.model';
+import { HttpClient } from '@angular/common/http';
+import { EquipmentService } from '../../services/equipment-service';
 
 @Component({
   selector: 'equipment-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './equipment-detail.html',
   styleUrl: './equipment-detail.css',
 })
-export class EquipmentDetail {}
+export class EquipmentDetail {
+  
+}
