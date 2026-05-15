0fa711e434ea65f828f7172f1b0cda96a3b9ccbf
Nicolas <nicolas.launay.0@gmail.com>
2026-05-15 10:15:37 +0200

    REFACTOR(data-class): relocalized in an enums folder

---
+++ src/app/enums/data-class.enum.ts
@@ -0,0 +1,14 @@
+export enum DataClass {
+    TYPES = "Types",
+    MODELS = "Modèles",
+    EQUIPMENTS = "Equipements",
+    USERS = "Utilisateurs",
+    ACCREDITATIONS = "Accréditations",
+    ROLES = "Rôles",
+    DOCUMENTATIONS = "Documentations",
+    LOCATIONS = "Localisations",
+    STATES = "Etats",
+    MODIFICATIONS = "Modifications",
+    LOANS = "Prêts",
+    REQUESTS = "Demandes"
+}
