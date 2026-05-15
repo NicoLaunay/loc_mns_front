f9d1c0e7313452e8437119afbabde1b41853a798
Nicolas <nicolas.launay.0@gmail.com>
2026-05-15 10:16:46 +0200

    NEW(admin-home): added and styled admin menu

--- src/app/pages/admin-home/admin-home.ts
+++ src/app/pages/admin-home/admin-home.ts
@@ -1,20 +1,22 @@
 import { Component, inject, Signal } from '@angular/core';
 import { EquipmentService } from '../../services/equipment-service';
 import { AdminEquipmentCard } from "../../layouts/admin-equipment-card/admin-equipment-card";
-import { DataClass } from '../../models/data-class.enum';
+import { DataClass } from '../../enums/data-class.enum';
 import { Equipment, TestEquipment } from '../../models/equipment';
 import { Observable } from 'rxjs';
-
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'admin-home',
-  imports: [AdminEquipmentCard],
+  imports: [AdminEquipmentCard, NgClass],
   templateUrl: './admin-home.html',
   styleUrl: './admin-home.css',
 })
 export class AdminHome {
 
   // Exposer l'enum au template
-  protected readonly DataClass = DataClass;
+  protected readonly dataClass = DataClass;
+  protected readonly dataClassEntries = Object.entries(this.dataClass) as [String, DataClass][]
+
   protected showedList: DataClass = DataClass.EQUIPMENTS
 
   private equipmentService = inject(EquipmentService)
@@ -24,13 +26,10 @@ export class AdminHome {
   ngOnInit(): void {
     this.equipmentService.getAll().subscribe()
     console.log(this.allEquipments);
-    
   }
 
   showList(list: DataClass): void {
     this.showedList = list
   }
-
-
 
 }
